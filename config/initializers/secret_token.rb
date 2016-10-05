f38694fe4eaddba19ba37ec4e2a79a5e25289bc0
Mohamed Siddhiq <msiddhiq@anpi.com>
2016-10-05 17:31:35 +0530

    Init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stay::Application.config.secret_key_base = '04edc4c54351e453f108fdb097cb4a014cb41d4405e44c6ddc0ff8e933c38320a879186a8cf40dacf2f49d3f9fb510a6c58fb48302f4f4abcc24b91524cce323'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Stay::Application.config.session_store :cookie_store, key: '_stay_session'
